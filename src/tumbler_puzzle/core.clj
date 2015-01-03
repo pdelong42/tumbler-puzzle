44dacab7fd28c92a669d3f0b36ce6f34f597ca6a
Paul DeLong <pd72@nyu.edu>
2015-01-03 14:32:45 -0500

    a first (naive) pass at transcribing the essential pieces of the puzzle into a data structure

--- src/tumbler_puzzle/core.clj
+++ src/tumbler_puzzle/core.clj
@@ -1,5 +1,20 @@
 (ns tumbler-puzzle.core (:gen-class))
 
+; Here's a first pass at the a data structure.  This is inadquate in many ways,
+; but I just wanted to get the basic structure transcribed for starters, and
+; make it more robust as this evolves.
+
+(def tumblers
+  #{  [1 3 4 2]
+      [+ * - /]
+      [1 3 2 4]
+      [+ / * -]
+      [1 4 3 2]
+      [+ - / *]
+      [1 2 4 3]
+      [= = = =]
+      [4 2 3 1]  }  )
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
