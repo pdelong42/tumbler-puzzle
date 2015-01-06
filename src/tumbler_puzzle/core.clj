52620c680f24e14d59ff1389b9abe65b562f2741
Paul DeLong <Paul.DeLong@gmail.com>
2015-01-06 11:11:49 -0500

    incremental improvement to the data representation

--- src/tumbler_puzzle/core.clj
+++ src/tumbler_puzzle/core.clj
@@ -1,19 +1,24 @@
 (ns tumbler-puzzle.core (:gen-class))
 
 ; Here's a first pass at the a data structure.  This is inadquate in many ways,
-; but I just wanted to get the basic structure transcribed for starters, and
-; make it more robust as this evolves.
+; but I just wanted to get the basic structure transcribed for starters, and ;
+; make it more robust as this evolves.  Update: I made a second pass - it's
+; still woefully inadequate, but it's slightly more useful.
 
 (def tumblers
-  #{  [1 3 4 2]
-      [+ * - /]
-      [1 3 2 4]
-      [+ / * -]
-      [1 4 3 2]
-      [+ - / *]
-      [1 2 4 3]
-      [= = = =]
-      [4 2 3 1]  }  )
+   {  :red    '(1 3 4 2)
+      :orange '(+ * - /)
+      :green  '(1 3 2 4)
+      :cyan   '(+ / * -)
+      :mango  '(1 4 3 2)
+      :mint   '(+ - / *)
+      :blue   '(1 2 4 3)
+      :white  '(= = = =)
+      :yellow '(4 2 3 1)  }  )
+
+(def digits [:red :green :mango :blue :yellow])
+
+(def operators [:orange :cyan :mint :white])
 
 (defn -main
   "I don't do a whole lot ... yet."
